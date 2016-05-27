7ee5e0ef8ad88cd81b8c3062f66f25e4b2d7680c
Kevin Eye <eye@buffalo.edu>
2016-05-26 21:24:24 -0400

    updated indentation style

--- MyButton.cpp
+++ MyButton.cpp
@@ -2,63 +2,63 @@
 #include <Scheduler.h>
 #include "MyButton.h"
 
-MyButton::MyButton(int buttonPin, void (*onPressCb)(MyButton*), void (*onReleaseCb)(MyButton*)) {
-  pin = buttonPin;
-  state = HIGH;
-  pressed = state == LOW;
-  lastValue = state;
-  lastDebounceTime = 0;
-  if (onPressCb) this->onPress(onPressCb);
-  if (onReleaseCb) this->onRelease(onReleaseCb);
-  buttons[numButtons++] = this;
+MyButton::MyButton(int buttonPin, void (*onPressCb)(MyButton *), void (*onReleaseCb)(MyButton *)) {
+    pin = buttonPin;
+    state = HIGH;
+    pressed = state == LOW;
+    lastValue = state;
+    lastDebounceTime = 0;
+    if (onPressCb) this->onPress(onPressCb);
+    if (onReleaseCb) this->onRelease(onReleaseCb);
+    buttons[numButtons++] = this;
 }
 
-MyButton* MyButton::buttons[MAX_BUTTONS];
+MyButton *MyButton::buttons[MAX_BUTTONS];
 int MyButton::numButtons = 0;
 
 void MyButton::readOne() {
-  int value = digitalRead(pin);
-  if (value != lastValue) {
-    lastDebounceTime = millis();
-  }
-  if ((millis() - lastDebounceTime) > 10) {
-    if (value != state){
-      state = value;
-      pressed = state == LOW;
-      if (pressCallback && pressed) pressCallback(this);
-      if (releaseCallback && !pressed) releaseCallback(this);
+    int value = digitalRead(pin);
+    if (value != lastValue) {
+        lastDebounceTime = millis();
     }
-  }
-  lastValue = value;
+    if ((millis() - lastDebounceTime) > 10) {
+        if (value != state) {
+            state = value;
+            pressed = state == LOW;
+            if (pressCallback && pressed) pressCallback(this);
+            if (releaseCallback && !pressed) releaseCallback(this);
+        }
+    }
+    lastValue = value;
 }
 
 void MyButton::setupOne() {
-  pinMode(pin, INPUT_PULLUP);
-  state = digitalRead(pin);
+    pinMode(pin, INPUT_PULLUP);
+    state = digitalRead(pin);
 }
 
 void MyButton::readAll() {
-  for (int i = 0; i < MyButton::numButtons; i++) {
-    MyButton::buttons[i]->readOne();
-  }
+    for (int i = 0; i < MyButton::numButtons; i++) {
+        MyButton::buttons[i]->readOne();
+    }
 }
 
 void MyButton::loop() {
-  MyButton::readAll();
-  delay(5);
+    MyButton::readAll();
+    delay(5);
 }
 
-void MyButton::onPress(void (*cb)(MyButton*)) {
-  pressCallback = cb;
+void MyButton::onPress(void (*cb)(MyButton *)) {
+    pressCallback = cb;
 }
 
-void MyButton::onRelease(void (*cb)(MyButton*)) {
-  releaseCallback = cb;
+void MyButton::onRelease(void (*cb)(MyButton *)) {
+    releaseCallback = cb;
 }
 
 void MyButton::setup() {
-  for (int i = 0; i < MyButton::numButtons; i++) {
-    MyButton::buttons[i]->setupOne();
-  }
-  Scheduler.start(NULL, MyButton::loop);
+    for (int i = 0; i < MyButton::numButtons; i++) {
+        MyButton::buttons[i]->setupOne();
+    }
+    Scheduler.start(NULL, MyButton::loop);
 }
--- MyButton.h
+++ MyButton.h
@@ -4,26 +4,37 @@
 #define MAX_BUTTONS 16
 
 class MyButton {
-  private:
-    static MyButton* buttons[MAX_BUTTONS];
+private:
+    static MyButton *buttons[MAX_BUTTONS];
     static int numButtons;
     int pin;
     int lastValue;
     unsigned long lastDebounceTime;
-    void (*pressCallback)(MyButton*);
-    void (*releaseCallback)(MyButton*);
+
+    void (*pressCallback)(MyButton *);
+
+    void (*releaseCallback)(MyButton *);
+
     void readOne();
+
     void setupOne();
+
     static void readAll();
+
     static void loop();
-  public:
-    MyButton(int pin, void (*onPress)(MyButton*)=NULL, void (*onRelease)(MyButton*)=NULL);
+
+public:
+    MyButton(int pin, void (*onPress)(MyButton *) = NULL, void (*onRelease)(MyButton *) = NULL);
+
     String label;
     int state;
     bool pressed;
+
     static void setup();
-    void onPress(void (*cb)(MyButton*));
-    void onRelease(void (*cb)(MyButton*));
+
+    void onPress(void (*cb)(MyButton *));
+
+    void onRelease(void (*cb)(MyButton *));
 };
 
 #endif
