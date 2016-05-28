6fd98cc011ecfcde7a9c82b81ae627c942a3b4d2
Kevin Eye <eye@buffalo.edu>
2016-05-27 22:26:41 -0400

    fixed some generic global names

--- KWiFi.cpp
+++ KWiFi.cpp
@@ -15,19 +15,19 @@ KWiFi::KWiFi(char *s, char *p) : KTask(5000) {
 
 #ifdef KWIFI_SERIAL_STATUS
 
-void on_wifi_disconnect(KEvent event, void *callbackData) {
+void KEvent_on_wifi_disconnect(KEvent event, void *callbackData) {
     Serial.println("WiFi disconnected.");
 };
 
-void on_wifi_connect(KEvent event, void *callbackData) {
+void KEvent_on_wifi_connect(KEvent event, void *callbackData) {
     Serial.println("WiFi connected.");
 };
 
-void on_wifi_failed(KEvent event, void *callbackData) {
+void KEvent_on_wifi_failed(KEvent event, void *callbackData) {
     Serial.println("WiFi connect failed.");
 };
 
-void on_wifi_connecting(KEvent event, void *callbackData) {
+void KEvent_on_wifi_connecting(KEvent event, void *callbackData) {
     Serial.println("WiFi attempting to connect...");
 };
 #endif
@@ -39,10 +39,10 @@ void KWiFi::init() {
 #endif
 
 #ifdef KWIFI_SERIAL_STATUS
-    KEvent::on(KEVENT_WIFI_CONNECT, on_wifi_connect, this);
-    KEvent::on(KEVENT_WIFI_CONNECTING, on_wifi_connecting, this);
-    KEvent::on(KEVENT_WIFI_CONNECT_FAILED, on_wifi_failed, this);
-    KEvent::on(KEVENT_WIFI_DISCONNECT, on_wifi_disconnect, this);
+    KEvent::on(KEVENT_WIFI_CONNECT, KEvent_on_wifi_connect, this);
+    KEvent::on(KEVENT_WIFI_CONNECTING, KEvent_on_wifi_connecting, this);
+    KEvent::on(KEVENT_WIFI_CONNECT_FAILED, KEvent_on_wifi_failed, this);
+    KEvent::on(KEVENT_WIFI_DISCONNECT, KEvent_on_wifi_disconnect, this);
 #endif
 }
 
