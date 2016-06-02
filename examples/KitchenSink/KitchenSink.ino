4cf8cf54e7bbd720f83d9e289ad02516ad9172e5
Kevin Eye <eye@buffalo.edu>
2016-06-02 00:01:50 -0400

    added more to kitchen sink

--- examples/KitchenSink/KitchenSink.ino
+++ examples/KitchenSink/KitchenSink.ino
@@ -1,69 +1,146 @@
 #include <SPI.h>
 #include <KTask.h>
+#include <KEvent.h>
 #include <KWiFi.h>
+#include <KHttpServer.h>
+#include <KMDNS.h>
+#include <KMqtt.h>
 #include <KButton.h>
 #include <KDisplay.h>
 
-#define BUTTON_A 9
-#define BUTTON_B 6
-#define BUTTON_C 5
+#define BUTTON_A    9
+#define BUTTON_B    6
+#define BUTTON_C    5
+#define LEDPIN      13
+
+#define WIFI_SSID   "ssid"
+#define WIFI_PASS   "pass"
+#define MQTT_SERVER "io.adafruit.com"
+#define MQTT_PORT   1883
+#define MQTT_USER   "username"
+#define MQTT_PASS   "aio-key"
+#define MDNS_NAME   "feather"
 
 KDisplay display;
+KWiFi wifi(WIFI_SSID, WIFI_PASS);
+Adafruit_WINC1500Client client;
 
-KWiFi wifi("abc", NULL);
+KHttpServer server(80);
+KMDNS mdns(MDNS_NAME);
 
-char *defaultText = "Press a button...";
+char ledState[2] = "0";
 
-// handler for button A
+const char MQTT_CLIENTID[] = __TIME__ MQTT_USER;
+KMqtt mqtt(&client, MQTT_SERVER, MQTT_PORT, MQTT_CLIENTID, MQTT_USER, MQTT_PASS);
+
+const char DISPLAY_FEED[] = MQTT_USER "/feeds/display";
+KMqttPublish displayPub = KMqttPublish(&mqtt, DISPLAY_FEED);
+
+void updateDisplay(KMqttSubscribe *sub, void *data) {
+    char *value = (char *) sub->lastread;
+    display.set(value);
+}
+
+KMqttSubscribe displaySub = KMqttSubscribe(&mqtt, DISPLAY_FEED, updateDisplay);
+
+void updateDisplay(void *);
+const char LED_FEED[] = MQTT_USER "/feeds/led";
+KMqttSubscribe ledSub = KMqttSubscribe(&mqtt, LED_FEED, updateLed);
+KMqttPublish ledPub = KMqttPublish(&mqtt, LED_FEED);
+void updateLed(KMqttSubscribe *sub, void *data) {
+    char *value = (char *) sub->lastread;
+    if (strcmp(value, "1") == 0) {
+        digitalWrite(LEDPIN, HIGH);
+        ledState[0] = '1';
+    } else {
+        digitalWrite(LEDPIN, LOW);
+        ledState[0] = '0';
+    }
+}
+
+void connect(KEvent event, void *callbackData) {
+    display.clear();
+    display.print("Connected to ");
+    display.println(wifi.wifi->SSID());
+    display.print("http://");
+    IPAddress ip = wifi.wifi->localIP();
+    display.println(ip);
+    display.print("http://");
+    display.print(mdns.name);
+    display.println(".local");
+    display.flush();
+};
+
+void request(KEvent event, void *callbackData) {
+    KHttpRequest *request = (KHttpRequest *) event.data;
+    ArduinoHttpServer::MethodEnum method(ArduinoHttpServer::MethodInvalid);
+    method = request->getMethod();
+    const String path = request->getResource().toString();
+    if (path == "/display") {
+        if (method == ArduinoHttpServer::MethodGet) {
+            ArduinoHttpServer::StreamHttpReply httpReply(request->getStream(), "text/plain");
+            httpReply.send(display.get());
+        } else if (method == ArduinoHttpServer::MethodPut) {
+            display.set((char *) request->getBody());
+            if (mqtt.connected()) displayPub.publish(display.get());
+            ArduinoHttpServer::StreamHttpReply httpReply(request->getStream(), "text/plain");
+            httpReply.send(display.get());
+        }
+    } else if (path == "/led") {
+        if (method == ArduinoHttpServer::MethodGet) {
+            ArduinoHttpServer::StreamHttpReply httpReply(request->getStream(), "text/plain");
+            httpReply.send(ledState);
+        } else if (method == ArduinoHttpServer::MethodPut) {
+            if (strcmp(request->getBody(), "1") == 0) {
+                digitalWrite(LEDPIN, HIGH);
+                ledState[0] = '1';
+            } else {
+                digitalWrite(LEDPIN, LOW);
+                ledState[0] = '0';
+            }
+            if (mqtt.connected()) ledPub.publish(ledState);
+            ArduinoHttpServer::StreamHttpReply httpReply(request->getStream(), "text/plain");
+            httpReply.send(ledState);
+        }
+    }
+};
+
+const char BUTTON_A_FEED[] = MQTT_USER "/feeds/button-a";
+KMqttPublish buttonAPub = KMqttPublish(&mqtt, BUTTON_A_FEED);
 void press_a(KButton *button) {
-    Serial.println("Press: A");
-    display.set("A");
+    if (mqtt.connected()) buttonAPub.publish((uint32_t) 1);
 }
-
 void release_a(KButton *button) {
-    Serial.println("Release: A");
-    display.set(defaultText);
+    if (mqtt.connected()) buttonAPub.publish((uint32_t) 0);
 }
-
 KButton buttonA(BUTTON_A, press_a, release_a);
 
-// handler for button B
+const char BUTTON_B_FEED[] = MQTT_USER "/feeds/button-b";
+KMqttPublish buttonBPub = KMqttPublish(&mqtt, BUTTON_B_FEED);
 void press_b(KButton *button) {
-    Serial.println("Press: B");
-    display.set("B");
+    if (mqtt.connected()) buttonBPub.publish((uint32_t) 1);
 }
-
 void release_b(KButton *button) {
-    Serial.println("Release: B");
-    display.set(defaultText);
+    if (mqtt.connected()) buttonBPub.publish((uint32_t) 0);
 }
-
 KButton buttonB(BUTTON_B, press_b, release_b);
 
-// handler for button C
+const char BUTTON_C_FEED[] = MQTT_USER "/feeds/button-c";
+KMqttPublish buttonCPub = KMqttPublish(&mqtt, BUTTON_C_FEED);
 void press_c(KButton *button) {
-    Serial.println("Press: C");
-    display.set("C");
+    if (mqtt.connected()) buttonCPub.publish((uint32_t) 1);
 }
-
 void release_c(KButton *button) {
-    Serial.println("Release: C");
-    display.set(defaultText);
+    if (mqtt.connected()) buttonCPub.publish((uint32_t) 0);
 }
-
 KButton buttonC(BUTTON_C, press_c, release_c);
 
+
 void setup() {
-    // initialize serial
-    // while (!Serial);
-    Serial.begin(9600);
-    Serial.println("Press a button...");
-
-    // initialize tasks
+    pinMode(LEDPIN, OUTPUT);
     KTask::setup();
-
-    // initialize display
-    display.set(defaultText);
+    KEvent::on(KEVENT_WIFI_CONNECT, connect);
+    KEvent::on(KEVENT_HTTPSERVER_REQUEST, request);
 }
 
 void loop() {
