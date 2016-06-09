3685484960ea41b30575a81fb09ecc000bcaa0a8
Kevin Eye <eye@buffalo.edu>
2016-06-08 20:03:44 -0400

    added ESO8266 example

---
+++ examples/ESP8266/ESP8266.ino
@@ -0,0 +1,49 @@
+#include <SPI.h>
+#include <KTask.h>
+
+#define LEDPIN LED_BUILTIN
+
+bool ledOn = false;
+void blink() {
+    if (ledOn) {
+        digitalWrite(LEDPIN, LOW);
+        Serial.println("on");
+        ledOn = false;
+    } else {
+        digitalWrite(LEDPIN, HIGH);
+        Serial.println("off");
+        ledOn = true;
+    }
+}
+
+KTask blinker(500, blink);
+
+void setup() {
+    Serial.begin(74880);
+    pinMode(LEDPIN, OUTPUT);
+    Serial.println("starting");
+    KTask::setup();
+}
+
+void loop() {
+    KTask::loop();
+}
+
+//#define LED LED_BUILTIN
+//
+//// the setup function runs once when you press reset or power the board
+//void setup() {
+//    // initialize digital pin 13 as an output.
+//    Serial.begin(74880);
+//    pinMode(LED, OUTPUT);
+//}
+//
+//// the loop function runs over and over again forever
+//void loop() {
+//    digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+//    Serial.println("off");
+//    delay(1000);              // wait for a second
+//    digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+//    Serial.println("on");
+//    delay(1000);              // wait for a second
+//}
