cce073dea58a2a2314f7d749c61a08692d0ae0e5
Kevin Eye <eye@buffalo.edu>
2016-06-08 22:17:54 -0400

    adapted KMQTT ESP8266 demo

--- examples/ESP8266/ESP8266.ino
+++ examples/ESP8266/ESP8266.ino
@@ -4,12 +4,27 @@
 #include <KHttpServer.h>
 #include <KEvent.h>
 #include <KMDNS.h>
+#include <KMQtt.h>
+
+#define WIFI_SSID   "ssid"
+#define WIFI_PASS   "pass"
+#define MQTT_SERVER "io.adafruit.com"
+#define MQTT_PORT   1883
+#define MQTT_USER   "username"
+#define MQTT_PASS   "aio-key"
+#define MDNS_NAME   "esp"
 
 #define LEDPIN LED_BUILTIN
 
-KWiFi wifi("ssid", "password");
+KWiFi wifi(WIFI_SSID, WIFI_PASS);
 KHttpServer server(80);
-KMDNS mdns("esp");
+KMDNS mdns(MDNS_NAME);
+
+#ifdef ARDUINO_ARCH_ESP8266
+    WiFiClient client;
+#else
+    Adafruit_WINC1500Client client;
+#endif
 
 bool ledOn = false;
 void blink() {
@@ -39,6 +54,30 @@ void request(KEvent event, void *callbackData) {
     ArduinoHttpServer::StreamHttpReply httpReply(request->getStream(), "application/json");
     httpReply.send("{\"foo\":\"bar\"}");
 };
+
+const char MQTT_CLIENTID[] = __TIME__ MQTT_USER;
+KMqtt mqtt(&client, MQTT_SERVER, MQTT_PORT, MQTT_CLIENTID, MQTT_USER, MQTT_PASS);
+
+const char PUB_FEED[] = MQTT_USER "/feeds/counter-pub";
+KMqttPublish counterPub = KMqttPublish(&mqtt, PUB_FEED);
+int32_t count = 0;
+
+void countUp() {
+    Serial.println(++count);
+    if (mqtt.connected()) counterPub.publish(count);
+}
+
+KTask counter(3000, countUp);
+
+void updateCount(void *);
+
+const char SUB_FEED[] = MQTT_USER "/feeds/counter-sub";
+KMqttSubscribe counterSub = KMqttSubscribe(&mqtt, SUB_FEED, updateCount);
+
+void updateCount(KMqttSubscribe *sub, void *data) {
+    char *value = (char *) sub->lastread;
+    count = atol(value);
+}
 
 void setup() {
     Serial.begin(74880);
